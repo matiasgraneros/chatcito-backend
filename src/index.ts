4159bca622ff26ed388d43aad2f773a041d9fb42
Matias <117133660+matiasgraneros@users.noreply.github.com>
2025-01-18 05:47:34 +0000

    CORS edited

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT;
 
 const corsOptions = {
-  origin: ['http://localhost:5173'],
+  origin: ['https://chatcito-frontend.vercel.app'],
   methods: ['POST', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
